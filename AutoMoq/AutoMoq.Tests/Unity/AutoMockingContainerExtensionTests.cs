fcebfac9677edb6a5e5040869d6c7caf7e54d88f
darrencauthon <darren@cauthon.com>
2010-01-09 15:33:42 -0600

    Add mocking container extensions.

---
+++ AutoMoq/AutoMoq.Tests/Unity/AutoMockingContainerExtensionTests.cs
@@ -0,0 +1,69 @@
+﻿using AutoMoq.Unity;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoMoq.Tests.Unity
+{
+
+    [TestClass]
+    public class AutoMockingContainerExtensionTests
+    {
+        [TestMethod]
+        public void CanCreateAMockInterface()
+        {
+            var container = new UnityContainer();
+            container.AddNewExtension<AutoMockingContainerExtension>();
+
+            var action = container.Resolve<IDependency>();
+        }
+
+        [TestMethod]
+        public void CanInstantiateAClassWithInterfaceDependency()
+        {
+            var container = new UnityContainer();
+            container.AddNewExtension<AutoMockingContainerExtension>();
+
+            var action = container.Resolve<Action>();
+        }
+
+        [TestMethod]
+        public void RegisteringAnInstanceWillForgoTheMockObject()
+        {
+            var container = new UnityContainer();
+            container.AddNewExtension<AutoMockingContainerExtension>();
+
+            container.RegisterInstance<IDependency>(new DependencyImplementation());
+
+            var dependency = container.Resolve<IDependency>();
+
+            Assert.AreEqual(typeof (DependencyImplementation), dependency.GetType());
+        }
+    }
+
+    public class Action
+    {
+        public Action(IDependency action, Today today)
+        {
+        }
+    }
+
+    public class Today
+    {
+        public Today(IDependency dependency)
+        {
+        }
+    }
+
+    public interface IDependency
+    {
+        int OnePlusOne();
+    }
+
+    public class DependencyImplementation : IDependency
+    {
+        public int OnePlusOne()
+        {
+            return -1;
+        }
+    }
+}
---
+++ AutoMoq/AutoMoq/Unity/AutoMockingBuilderStrategy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Practices.ObjectBuilder2;
+using Moq;
+
+namespace AutoMoq.Unity
+{
+    public class AutoMockingBuilderStrategy : BuilderStrategy
+    {
+        private readonly MockFactory mockFactory;
+        private readonly IEnumerable<Type> registeredTypes;
+
+        public AutoMockingBuilderStrategy(IEnumerable<Type> registeredTypes)
+        {
+            mockFactory = new MockFactory(MockBehavior.Loose);
+            this.registeredTypes = registeredTypes;
+        }
+
+        public override void PreBuildUp(IBuilderContext context)
+        {
+            var type = GetTheTypeFromTheBuilderContext(context);
+            if (AMockObjectShouldBeCreatedForThisType(type))
+                context.Existing = CreateAMockObject(type).Object;
+        }
+
+        #region private methods
+
+        private bool AMockObjectShouldBeCreatedForThisType(Type type)
+        {
+            return TypeIsNotRegistered(type) && type.IsInterface;
+        }
+
+        private static Type GetTheTypeFromTheBuilderContext(IBuilderContext context)
+        {
+            return ((NamedTypeBuildKey)context.OriginalBuildKey).Type;
+        }
+
+        private bool TypeIsNotRegistered(Type type)
+        {
+            return registeredTypes.Any(x => x.Equals(type)) == false;
+        }
+
+        private Mock CreateAMockObject(Type type)
+        {
+            var createMethod = GenerateAnInterfaceMockCreationMethod(type);
+
+            return InvokeTheMockCreationMethod(createMethod);
+        }
+
+        private Mock InvokeTheMockCreationMethod(MethodInfo createMethod)
+        {
+            return (Mock)createMethod.Invoke(mockFactory, new object[] {new List<object>().ToArray()});
+        }
+
+        private MethodInfo GenerateAnInterfaceMockCreationMethod(Type type)
+        {
+            var createMethodWithNoParameters = mockFactory.GetType().GetMethod("Create", EmptyArgumentList());
+
+            return createMethodWithNoParameters.MakeGenericMethod(new[] {type});
+        }
+
+        private static Type[] EmptyArgumentList()
+        {
+            return new[] {typeof (object[])};
+        }
+
+        #endregion
+    }
+}
