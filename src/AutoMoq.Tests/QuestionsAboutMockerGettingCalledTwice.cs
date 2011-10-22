747854aad3019cea460ab478c8b5afffaa1cae2f
Darren Cauthon <darren@cauthon.com>
2011-10-22 08:34:47 -0500

    Rename test.

---
+++ src/AutoMoq.Tests/QuestionsAboutMockerGettingCalledTwice.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace AutoMoq.Tests
+{
+    [TestFixture]
+    public class QuestionsAboutMockerGettingCalledTwice
+    {
+        [Test]
+        public void Can_a_mock_generated_by_automoq_be_called_twice()
+        {
+            var mocker = new AutoMoqer();
+
+            var id = "the id";
+            var profile = new Profile();
+            var profiles = mocker.Create<SomeController>();
+
+            mocker.GetMock<IProfilerGetter>().Setup(p => p.Get(id)).Returns(profile);
+            var p1 = profiles.Get(id);
+            var p2 = profiles.Get(id);
+            Assert.IsNotNull(p1);
+            Assert.IsNotNull(p2);
+        }
+
+        public class SomeController
+        {
+            private readonly IProfilerGetter profilerGetter;
+
+            public SomeController(IProfilerGetter profilerGetter)
+            {
+                this.profilerGetter = profilerGetter;
+            }
+
+            public Profile Get(string id)
+            {
+                return profilerGetter.Get(id);
+            }
+        }
+
+        public interface IProfilerGetter
+        {
+            Profile Get(string id);
+        }
+
+        public class Profile
+        {
+        }
+    }
+}
