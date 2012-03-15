65ac397b57b011f4fa4c92aa477c14e683eb0cf3
kay.one <kay.one@gmail.com>
2012-03-14 22:04:50 -0700

    Updated assembly info

--- src/AutoMoq/Properties/AssemblyInfo.cs
+++ src/AutoMoq/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("AutoMoq")]
+[assembly: AssemblyTitle("Kayone.AutoMoq")]
 [assembly: AssemblyDescription("Automocker for Moq.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DEG")]
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("e6f813c5-0225-4f79-824c-ac21b49decc9")]
+[assembly: Guid("763D9245-9999-4F46-A3FC-FCC97D4582CB")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
