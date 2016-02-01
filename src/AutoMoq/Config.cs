85587d827f6c19756017db167ecd109e1f5b336f
Darren Cauthon <darren@cauthon.com>
2016-01-31 21:48:28 -0600

    Cleanup.

--- src/AutoMoq/Config.cs
+++ src/AutoMoq/Config.cs
@@ -5,13 +5,13 @@ namespace AutoMoq
 {
     public class Config
     {
-        public MockBehavior MockBehavior { get; set; }
-        public IUnityContainer Container { get; set; }
-
         public Config()
         {
             MockBehavior = MockBehavior.Loose;
             Container = new UnityContainer();
         }
+
+        public MockBehavior MockBehavior { get; set; }
+        public IUnityContainer Container { get; set; }
     }
 }
