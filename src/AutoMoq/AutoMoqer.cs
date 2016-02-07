b18a300a20d57fed6b53e321b0dcfff07ddc0b87
Darren Cauthon <darren@cauthon.com>
2016-02-06 22:31:12 -0600

    Cleanup.

--- src/AutoMoq/AutoMoqer.cs
+++ src/AutoMoq/AutoMoqer.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
-using AutoMoq.Unity;
 using Microsoft.Practices.Unity;
 using Moq;
 using Moq.Language.Flow;
