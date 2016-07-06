e9b4335292edeb41bf927be5c1b82d3c92e066b8
Darren Cauthon <darren@cauthon.com>
2016-07-05 23:39:19 -0500

    I just do not know.

--- src_dotnetcore/AutoMoq/src/AutoMoq.Tests/ConfigTests.cs
+++ src_dotnetcore/AutoMoq/src/AutoMoq.Tests/ConfigTests.cs
@@ -1,20 +1,24 @@
-//using Moq;
-//using NUnit.Framework;
-//using Should;
+using System;
+using Moq;
+using Xunit;
 
-//namespace AutoMoq.Tests
-//{
-//    public class ConfigTests
-//    {
-//        [TestFixture]
-//        public class MockBehaviorTests
-//        {
-//            [Test]
-//            public void It_should_default_to_loose()
-//            {
-//                var config = new Config();
-//                config.MockBehavior.ShouldEqual(MockBehavior.Loose);
-//            }
-//        }
-//    }
-//}
+//using NUnit.Framework;
+
+namespace AutoMoq.Tests
+{
+    public class ConfigTests
+    {
+        //[TestFixture]
+        public class MockBehaviorTests
+        {
+            [Fact]
+            public void It_should_default_to_loose()
+            {
+                var config = new Config();
+                //Assert.AreEqual(MockBehavior.Loose, config.MockBehavior);
+                if (config.MockBehavior != MockBehavior.Loose)
+                    throw new Exception("uh oh!");
+            }
+        }
+    }
+}
